ae086acbfb44e890019ff9238f772b9ab24ce4f7
neelkamalkaur <144261030+neelkamalkaur@users.noreply.github.com>
2023-09-11 00:37:53 +0530

    Update CoverHistory.jsx

--- src/Components/CoverHistory/CoverHistory.jsx
+++ src/Components/CoverHistory/CoverHistory.jsx
@@ -14,10 +14,26 @@ export default function CoverHistory() {
 
   return (
     <>
-     <h1>Cover History</h1>
-     <button onClick={()=>{
+       <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-lg-12">
+            <div class="quiz-header-title mb-5">
+            <h1>Cover History</h1>
+
+            </div>
+     
+     
+     <div class="quiz-body">
+     <div class="d-grid my-4">
+     <button class="btn btn-black btn-block rounded-pill" onClick={()=>{
         navigate('/userallergicinfo');
      }}>Continue</button>
+     </div>
+     </div>
+     </div>
+     </div>
+     </div>
+    
 
     </>
   )
